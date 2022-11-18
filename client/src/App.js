7c22267e01abc70aa0412baf48ce1c616ec1ee3a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-18 16:41:43 +0500

    create basic file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Chat from './components/Chat';
+import Join from './components/Join';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
 }
 
