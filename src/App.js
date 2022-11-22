e2160f15579e68a8c99d7fb15256d27c9d0f917b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-22 15:21:39 +0500

    add dynamic functions, and login form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from "react-chat-engine";
-import ChatFeed from './components/ChatFeed'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
     <ChatEngine
       height="100vh"
-      projectID="e2faa274-eecf-4cfe-98bb-1b108b1cd4b3"
-      userName="BakshInternational"
-      userSecret="BakshInternational"
+      projectID="51b030a0-4b4c-4e2b-a773-543a23fb99f6"
+      userName="Fatima"
+      userSecret="456"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,19 +1,20 @@
 const MyMessage = ({ message }) => {
-  if (message?.attachements?.length > 0) {
+  if (message.attachments && message.attachments.length > 0) {
     return (
       <img
-        src={message.attachements[0].file}
+        src={message.attachments[0].file}
         alt="message-attachment"
         className="message-image"
-        style= {{float: 'right'}}
+        style={{ float: 'right' }}
       />
-    )
+    );
   }
+
   return (
     <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
       {message.text}
     </div>
-  )
-}
+  );
+};
 
-export default MyMessage
+export default MyMessage;
