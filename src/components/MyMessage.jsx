8f37ea45f6e2f297e98292869f8da614cabe3736
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-22 12:29:35 +0500

    create mymessage & their message feed

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,6 +1,18 @@
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+  if (message?.attachements?.length > 0) {
+    return (
+      <img
+        src={message.attachements[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style= {{float: 'right'}}
+      />
+    )
+  }
   return (
-    <div>MyMessage</div>
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+      {message.text}
+    </div>
   )
 }
 
